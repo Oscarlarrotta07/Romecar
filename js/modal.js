cb5fe412902e24427a49bf8764bcd2e3f3bc33de
Oscar Larrotta <oscarlarrotta@hotmail.com>
2021-07-16 23:27:48 -0500

    actualizacion de imagenes

--- js/modal.js
+++ js/modal.js
@@ -1,4 +1,5 @@
-let imagenes = document.querySelectorAll('.gallery__img');
+/*let imagenes = document.querySelectorAll('.gallery__img');
+let texto = document.querySelectorAll('.gallery__txt');
 let modal = document.querySelector('#modal');
 let img = document.querySelector('#modal__img');
 let boton = document.querySelector('#modal__boton');
@@ -12,4 +13,29 @@ for (let i = 0; i < imagenes.length; i++) {
 };
 boton.addEventListener('click', function() {
     modal.classList.toggle("modal--open");
+});*/
+
+let imagenes = document.querySelectorAll('.gallery__figure');
+let modal = document.querySelector('#modal__img');
+let texto = document.querySelector('#modal__txt');
+let modal1 = document.querySelector('.modal');
+let boton = document.querySelector('.modal__boton');
+imagenes.forEach(element => {
+    element.addEventListener('click', function() {
+        let txt = element.querySelector('.gallery__txt');
+        let img = element.querySelector('.gallery__img');
+        let src = img.getAttribute("src");
+        let contenido = txt.innerHTML;
+        modal.setAttribute("src", src);
+        texto.innerHTML = contenido;
+
+        modal1.classList.add("modal--open");
+
+    })
+
+
+});
+boton.addEventListener('click', function() {
+    modal1.classList.remove("modal--open");
+
 });
